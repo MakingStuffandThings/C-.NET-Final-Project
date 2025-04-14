daea3a7f727f977eeb996a2b9f1c55f6af514451
MakingStuffandThings <patrickgardner4@gmail.com>
2025-04-13 19:18:23 -0500

    Initial Commit

---
+++ NetworkingSocketProject/Program.cs
@@ -0,0 +1,209 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.IO;
+
+class Server
+{
+    static void Main()
+    {
+        string filepath = "../../../users.txt";
+        Dictionary<string, string> userData = new Dictionary<string, string>();
+        string currentUser = "none";
+
+
+        if (File.Exists(filepath))
+        {
+            foreach (string item in File.ReadAllLines(filepath))
+            {
+                string fixedString=item.Replace("(", "").Replace(")", "").Replace(" ","");
+                string userName=fixedString.Split(",")[0];
+                string userPassword = fixedString.Split(",")[1];
+                userData.Add(userName, userPassword);
+
+               // Console.WriteLine(fixedString+"Username=: "+userName+" Password=: "+userPassword);
+            }
+
+        }
+        else 
+        {
+            Console.WriteLine("File does not exist,check your filepath, you are in :"+Directory.GetCurrentDirectory()); 
+        }
+        
+
+
+
+
+        int port = 11627; //port had to be shorter
+        TcpListener listener = new TcpListener(IPAddress.Loopback, port);
+        try
+        {
+
+
+            listener.Start();
+            Console.WriteLine("Server is listening...");
+            // Accept client connection
+            TcpClient client = listener.AcceptTcpClient();
+            Console.WriteLine("Client connected!");
+            // Handle data
+            NetworkStream stream = client.GetStream();
+
+            while (true)
+            {
+                try
+                {
+
+                        byte[] buffer = new byte[1024];
+                        int bytesRead = stream.Read(buffer, 0, buffer.Length);
+                        if(bytesRead == 0 )
+                        {
+                    
+                            Console.WriteLine("Client disconnected");
+                            //break;
+                        }
+                        string receivedMessage = Encoding.ASCII.GetString(buffer, 0, bytesRead);
+                        
+
+                        if(receivedMessage == "shutdown")
+                        {
+                            Console.WriteLine("Shutdown recieved, server shutting down");
+                            break;
+                        }
+
+                        if (string.IsNullOrEmpty(receivedMessage.Trim() )  )
+                        {
+                            Console.WriteLine("Empty msg from client");
+                            string response = "Empty msg from client";
+                            byte[] responseBytes = Encoding.ASCII.GetBytes(response);
+                            stream.Write(responseBytes, 0, responseBytes.Length);
+                        }
+                        else
+                        {
+                            Console.WriteLine($"Received: {receivedMessage}");
+
+                            // Respond to client
+                          /*  string response = "Message received!";
+                            byte[] responseBytes = Encoding.ASCII.GetBytes(response);
+                            stream.Write(responseBytes, 0, responseBytes.Length);
+                        */}
+                    //response pattern not defined by document
+                    //standard response pattern for output will be as follows
+                    //"Username" "requestType" "granted/denied"
+                    //example
+                    //george login granted
+                    //george newuser denied
+
+
+
+                    //for login, pattern should be as follows
+                    //"login" "userName" "userPassword"
+                    string[] receivedMessageTokens=receivedMessage.Split(" ");
+                    Console.WriteLine(receivedMessageTokens.Length);
+                    for (int i = 0; i < receivedMessageTokens.Length; i++)
+                    {
+                        receivedMessageTokens[i] = receivedMessageTokens[i].Trim();
+                    }
+                    //segment that validates login
+                    if (receivedMessageTokens.Length == 3 && receivedMessageTokens[0] == "login" && userData.GetValueOrDefault(receivedMessageTokens[1]) == receivedMessageTokens[2])
+                    { //checks if login message is of right length, checks if message starts with login, uses 2nd token as key to look in dictionary
+                      //if retrived value is equal to supplied password, then login is successful
+                        Console.WriteLine($"Login Sucessful for: {receivedMessage}");
+                        currentUser = receivedMessageTokens[1];
+                        // Respond to client
+                        string response = receivedMessageTokens[1] + " " + "login granted";
+                        byte[] responseBytes = Encoding.ASCII.GetBytes(response);
+                        stream.Write(responseBytes, 0, responseBytes.Length);
+                    }
+                    else if(receivedMessageTokens.Length == 3 && receivedMessageTokens[0] == "login")
+                    { 
+                        Console.WriteLine($"Login NOT sucessful for: {receivedMessage}");
+                        //currentUser = receivedMessageTokens[1];
+                        // Respond to client
+                        string response = receivedMessageTokens[1] + " " + "login denied";
+                        byte[] responseBytes = Encoding.ASCII.GetBytes(response);
+                        stream.Write(responseBytes, 0, responseBytes.Length);
+                    }
+
+                    //segment that validates newuser
+                    //formatt is as follows
+                    //newuser user pass
+                    if (receivedMessageTokens.Length == 3 && receivedMessageTokens[0] == "newuser" && !userData.ContainsKey(receivedMessageTokens[1]) )
+                    { //checks if newuser message is of right length, checks if message starts with newuser, uses 2nd token as key to look in dictionary
+                      //if retrived value is already contained in discionary, thats bad, and we cant make a new user
+                        Console.WriteLine($"newuser Sucessful for: {receivedMessage}");
+
+                        //currentUser = receivedMessageTokens[1];
+                        userData.Add(receivedMessageTokens[1], receivedMessageTokens[2]);
+                        File.AppendAllText(filepath,"\n"+"(" + receivedMessageTokens[1] + ", " + receivedMessageTokens[2]+")"   );
+                        // Respond to client with good news
+                        string response = receivedMessageTokens[1] + " " + "newuser granted";
+                        byte[] responseBytes = Encoding.ASCII.GetBytes(response);
+                        stream.Write(responseBytes, 0, responseBytes.Length);
+                    }
+                    else if(receivedMessageTokens.Length == 3 && receivedMessageTokens[0] == "newuser")
+                    {
+                        Console.WriteLine($"newuser NOT sucessful for: {receivedMessage}");
+                        //currentUser = receivedMessageTokens[1];
+                        // Respond to client with bad news :(
+                        string response = receivedMessageTokens[1] + " " + "newuser denied";
+                        byte[] responseBytes = Encoding.ASCII.GetBytes(response);
+                        stream.Write(responseBytes, 0, responseBytes.Length);
+                    }
+                    
+                    
+                    //validate send message 
+                    if (receivedMessageTokens.Length >= 2 && receivedMessageTokens[0] == "send")
+                    { //checks if send message is of right length, checks if message starts with send
+                        Console.WriteLine($"send Sucessful for: {receivedMessage}");
+
+                        //currentUser = receivedMessageTokens[1];
+                        // Respond to client with good news
+
+                        string response ="send "+currentUser+" "+receivedMessage;
+                        //string response= ">"+currentUser+": " + receivedMessage.Replace(currentUser,"").Remove(receivedMessage.IndexOf("send"),"send".Length).Trim();
+                        byte[] responseBytes = Encoding.ASCII.GetBytes(response);
+                        stream.Write(responseBytes, 0, responseBytes.Length);
+                    }
+                    else if (receivedMessageTokens.Length >=2 && receivedMessageTokens[0] == "send")
+                    {
+                        Console.WriteLine($"send NOT sucessful for: {receivedMessage}");
+                        //currentUser = receivedMessageTokens[1];
+                        // Respond to client with bad news :(
+                        string response = receivedMessageTokens[1] + " " + "newuser denied";
+                        byte[] responseBytes = Encoding.ASCII.GetBytes(response);
+                        stream.Write(responseBytes, 0, responseBytes.Length);
+                    }
+
+
+                    
+
+
+
+
+
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine("Client Disconnected Unexpectedly Error:" );
+                    Console.WriteLine("Listening");
+                    client = listener.AcceptTcpClient();
+                    Console.WriteLine("Client connected!");
+                    // Handle data
+                    stream = client.GetStream();
+                    //throw;
+                }
+
+
+
+            }
+            Console.WriteLine("Shutting down server");
+            client.Close(); // Close the client connection
+        }
+        catch (Exception e)
+        {
+
+            Console.Write("Error, exception" + e);
+        }
+    }
+}
---
+++ NetworkingSocketProjectClient/Program.cs
@@ -0,0 +1,192 @@
+﻿using System;
+using System.Net.Sockets;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Linq;
+
+class Client
+{
+    static void Main()
+    {
+        string server = "127.0.0.1"; // Loopback address
+        int port = 11627; // Same port as server my student ID is too long for this api so i shortened it
+        bool isLoggedIn=false;
+        string currentUser="none";
+
+
+        string userNamePattern= "^[a-zA-Z0-9]{3,32}$";//Regex for username, must be alphanum upper or lower, must be bewtwen 3-32 chars
+        string userPasswordPattern= "^[a-zA-Z0-9]{4,8}$";//Regex for password  must be alphanum upper or lower, must be bewtwen 4-8 chars
+        string messagePattern = "^[a-zA-Z0-9]{1,256}";//Regex for message.  must be alphanum upper or lower, must be bewtwen 1-265 chars
+
+
+
+        try
+        {
+            using (TcpClient client = new TcpClient(server, port))
+            {
+                NetworkStream stream = client.GetStream();
+               // Console.WriteLine("Connected to server!");
+                string control = "enter";
+
+
+                Console.WriteLine("My chat room client. Version One.\n");
+
+              
+
+                   
+                   
+                while (control != "logout")
+                {
+                    Console.Write(">");
+                    control = Console.ReadLine();
+                    //handle logic here to validate user input
+                    string[] response=control.Split(' ');
+
+
+                    bool isValidResponse=false;
+
+                    while (!isValidResponse)
+                    {   //login client input validation
+                        if (response.Length == 3 && !isLoggedIn && response[0] == "login" && Regex.IsMatch(response[1], userNamePattern) && Regex.IsMatch(response[2], userPasswordPattern))
+                        {
+                           // Console.WriteLine("Username and password is in valid formatt");
+                            break;
+                        }
+                        else { //Console.WriteLine("Username and Password ARE NOT in valid formatt and loged in is"+isLoggedIn); 
+                        }
+
+                        //newuser client input validation
+                        if (response.Length == 3 && !isLoggedIn && response[0] == "newuser" && Regex.IsMatch(response[1], userNamePattern) && Regex.IsMatch(response[2], userPasswordPattern))
+                        {
+                           // Console.WriteLine(" NEW Username and password is in valid formatt");
+                            break;
+                        }
+                        else { //Console.WriteLine(" NEW Username and Password ARE NOT in valid formattand loged in is" + isLoggedIn);
+                              }
+                        
+
+                        //send
+                        if (response.Length >=2 && isLoggedIn && response[0] == "send"  && Regex.IsMatch(response[1],messagePattern) )
+                        {
+                            //Console.WriteLine("sending your msg");
+                            break;
+                        }
+                        else if(!isLoggedIn)
+                        {
+                            Console.WriteLine("Denied. Please login first"); 
+                            //Console.WriteLine("Didnt send your msgand loged in is" + isLoggedIn);
+                        
+                        }
+
+                        if (control.Trim() == "logout")
+                        { break; }
+
+
+
+
+
+
+
+
+                        Console.Write(">");
+                        control = Console.ReadLine();
+                        response = control.Split(' ');
+
+                        
+
+                    }
+
+                    if (control.Trim() == "logout")
+                    { break; }
+
+
+
+
+
+
+
+
+
+
+
+                    control = control + "\n";
+                    // Send message to server
+                    string message = control;
+                    byte[] data = Encoding.ASCII.GetBytes(message);
+                    stream.Write(data, 0, data.Length);
+                   // Console.WriteLine("Sent: " + message);
+
+                    // Receive response from server
+                    byte[] buffer = new byte[1024];
+                    int bytesRead = stream.Read(buffer, 0, buffer.Length);
+                    string serverResponse = Encoding.ASCII.GetString(buffer, 0, bytesRead);
+                    
+                    
+                    
+                    
+                    
+                    //Console.WriteLine(serverResponse);
+                    
+                    
+                    
+                    
+                    
+                    control = control.Trim();
+                    string[] serverResponseTokens = serverResponse.Split(' ');
+
+                    //data sent back from server, handle for login, and new user verification
+                    //response pattern not defined by document
+                    //standard response pattern will be as follows
+                    //"Username" "requestType" "granted/denied"
+                    //example
+                    //george login granted
+                    //george newuser denied
+                    //no response from server about message denial request as client handles verification for valid send or not
+                    if (serverResponseTokens.Length == 3 && serverResponseTokens[1] == "login" && serverResponseTokens[2] == "granted")
+                    {//if server validates login, set islogged in to true
+                        isLoggedIn=true;
+                        currentUser = serverResponseTokens[0];
+                        //Console.WriteLine("Welcome: " + serverResponseTokens[0] + " You have successfully logged in"); 
+                        Console.WriteLine("> login confirmed");
+                    }
+                    else if(serverResponseTokens.Length == 3 && serverResponseTokens[1] == "login" && serverResponseTokens[2] == "denied")
+                    {
+                        Console.WriteLine("> Denied. User name or password incorrect.");
+                    }
+
+                    if (serverResponseTokens.Length == 3 && serverResponseTokens[1] == "newuser" && serverResponseTokens[2] == "granted")
+                    {//if server validates newuser
+                        //Console.WriteLine("Welcome: " + serverResponseTokens[0] + " You have successfully made a new account, please log in");
+                        Console.WriteLine("> New user account created. Please login.");
+                    }
+                    else if (serverResponseTokens.Length == 3 && serverResponseTokens[1] == "newuser" && serverResponseTokens[2] == "denied")
+                    {
+                        Console.WriteLine("> Denied. User account already exists.");
+                    }
+                    
+                                    
+                    if(serverResponseTokens.Length >=3 && serverResponseTokens[0]=="send" && serverResponseTokens[1]==currentUser)
+                    {
+
+
+                        Console.WriteLine(">"+string.Join(" ", serverResponseTokens[1])+": "+string.Join(" ",serverResponseTokens[3..]).TrimEnd());
+                        //Console.WriteLine("REACHED HERE");
+                        //Console.WriteLine(serverResponse.Remove(serverResponse.IndexOf("send"), "send".Length).Trim().Remove(server.IndexOf(currentUser),currentUser.Length));
+                    }
+                    //Console.WriteLine("ASDASDASDASD   " + serverResponseTokens[0]+" current user:"+currentUser);
+
+
+
+
+
+
+                }
+                Console.WriteLine(">" + currentUser + " left");
+            }
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine("Error: " + e.Message);
+        }
+    }
+}
